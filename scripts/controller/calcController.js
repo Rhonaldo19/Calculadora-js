720a834f1cb4a98ea6134506062224f755a33109
RhonaldoParente <rhonaldoparente@betanialacteos.com.br>
2021-03-12 17:07:31 -0300

    alterações calcController

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,30 +1,63 @@
 class CalcController {
 
     constructor() {
+        
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display")
         this._dateEl = document.querySelector("#data")
         this._timeEl = document.querySelector("#hora")
         this._currentDate
         this.initialize()
+        this.initButtonsEvents()
     }
 
     initialize() {
 
-        setDisplayDateTime()
+        this.setDisplayDateTime()
 
         setInterval(() => {
 
-            setDisplayDateTime()
+            this.setDisplayDateTime()
 
         }, 1000)
 
     }
 
     setDisplayDateTime() {
+
         this.displayDate = this.currentDate.toLocaleDateString(this._locale)
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
     }
+
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false)
+
+        })
+
+    }
+
+    initButtonsEvents() {
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag', e => {
+
+                console.log(btn.className.baseVal.replace('btn-', ''))
+            })
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = 'pointer'
+            })
+
+        })
+    }
+
 
     get displayTime() {
         return this._timeEl.innerHTML
