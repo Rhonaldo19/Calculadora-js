f4f45dba7fdb6e75a617adfb17100bd0c805c303
RhonaldoParente <rhonaldoparente@betanialacteos.com.br>
2021-03-19 17:40:37 -0300

    Tratando erros

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,9 @@ class CalcController {
 
     constructor() {
 
+        this._audio = new Audio('click.mp3')
+        this._audioOnOff = false
+
         this._lastOperator = ''
         this._lastNumber = ''
 
@@ -14,21 +17,21 @@ class CalcController {
         this.initialize()
         this.initButtonsEvents()
         this.initKeyboard()
-        
+
     }
 
-    pasteFromClipboard(){
+    pasteFromClipboard() {
 
-        document.addEventListener('paste', e=>{
+        document.addEventListener('paste', e => {
 
             let text = e.clipboardData.getData('Text')
-        
+
             this.displayCalc = parseFloat(text)
             console.log(text)
         })
     }
 
-    copyToClipboard(){
+    copyToClipboard() {
 
         let input = document.createElement('input')
 
@@ -56,11 +59,37 @@ class CalcController {
         this.setLastNumberToDisplay()
         this.pasteFromClipboard()
 
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+
+            btn.addEventListener('dblclick', e => {
+
+                this.toggleAudio()
+
+            })
+        })
+
+    }
+
+    toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff
+
+    }
+
+    playAudio() {
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0
+            this._audio.play()
+        }
     }
 
     initKeyboard() {
 
         document.addEventListener('keyup', e => {
+
+            this.playAudio()
 
             switch (e.key) {
 
@@ -100,7 +129,7 @@ class CalcController {
 
                 case 'c':
                     if (e.ctrlKey) this.copyToClipboard()
-                    break    
+                    break
             }
         })
 
@@ -167,7 +196,13 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""))
+        try {
+            return eval(this._operation.join(""))
+        } catch (e) {
+            setTimeout(()=>{
+            this.setError()
+        }, 1)
+        }
     }
 
     calc() {
@@ -297,6 +332,8 @@ class CalcController {
 
     execBtn(value) {
 
+        this.playAudio()
+
         switch (value) {
 
             case 'ac':
@@ -391,6 +428,12 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+            this.setError()
+            return false
+        }
+
         this._displayCalcEl.innerHTML = value
     }
 
